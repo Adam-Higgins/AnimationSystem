ebd5dd1b38f90f2d961f5b852393d63cdbd344af
Adam-Higgins <adamskeee@hotmail.co.uk>
2022-02-23 20:52:05 +0000

    Implemented Transform tracks, Pose class, Clip class
    Implemented the transform track class which is a higher level than the normal track class. Pose class hold the current data for a frame of an animation for all bones. The clip class holds all information about a single animation clip. added functions to the loader to load in the rest pose for a model and to load in all the animation clips from a file

--- AnimationProject/GLTFLoader.h
+++ AnimationProject/GLTFLoader.h
@@ -4,8 +4,13 @@
 #define _H_GLTFLOADER_
 
 #include "cgltf.h"
+#include "Pose.h"
+#include "Clip.h"
 
 cgltf_data* LoadGLTFFile(const char* path);
+Pose LoadRestPose(cgltf_data* data);
+std::vector<std::string> LoadJointNames(cgltf_data* data);
+std::vector<Clip> LoadAnimationClips(cgltf_data* data);
 void FreeGLTFFile(cgltf_data* handle);
 
 #endif
